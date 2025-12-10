f8bde16d465ee5e6d27ae5294ab3175ddc5b4bff
coreltech <coreltech.soluciones@gmail.com>
2025-12-09 23:44:39 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+// sw.js
+const CACHE_NAME = 'chefportable-v3';
+const urlsToCache = [
+  './',
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
