15f68eeb224b667ccd7b452eb4d4c052f012b037
coreltech <coreltech.soluciones@gmail.com>
2025-12-11 18:45:58 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,6 +6,7 @@ const CACHE_NAME = 'chefportable-v4';
 const urlsToCache = [
   './',
   './index.html',
+  './manifest.json',
   // Recursos externos necesarios
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://cdn.jsdelivr.net/npm/chart.js'
